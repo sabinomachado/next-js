b9baee49bcf250021621f55303a65066dae7731b
Sabino Machado <sabino_fjv@hotmail.com>
2026-02-10 23:05:22 -0300

    Config GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+
+  basePath: "/next-js",
+  assetPrefix: "/next-js/",
+
+  images: {
+    unoptimized: true
+  }
 };
 
 export default nextConfig;
