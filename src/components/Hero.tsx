c1a21af7fad9e584ecae89b70288a552b244ed85
Sabino Machado <sabino_fjv@hotmail.com>
2026-02-11 00:25:19 -0300

    change bkg imag

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -13,7 +13,7 @@ const Hero = () => {
       {/* Background image */}
       <div className="absolute inset-0">
         <Image
-          src="/next-js/hero-construction.jpg"
+          src="/next-js/hero-construction.png"
           alt="Prelaje - Construção e pré-moldados"
           fill
           className="object-cover"
